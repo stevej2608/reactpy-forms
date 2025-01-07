e6d55514351ba23b33c609dca564b92f80ded2b3
Steve Jones <jonesst2608@gmail.com>
2025-01-07 08:50:58 +0000

    Bump version (0.0.7)

--- reactpy_forms/__init__.py
+++ reactpy_forms/__init__.py
@@ -5,4 +5,4 @@ from reactpy_forms.forms_core import create_form, use_form_state
 from reactpy_forms.field_model import FieldModel, FieldValidationError
 from reactpy_forms.form_model import FormModel
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
